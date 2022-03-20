edb1f646126fcff37a003f106ca6664bcecd4ccb
Young <tcher.2009@gmail.com>
2022-03-20 15:26:14 +0800

    完成後台丟值前端顯示及前台透過路由參數丟值到其他前台顯示

---
+++ LearnTest0316/BLL/MVCAchPOC_DB_BLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using LearnTest0316.DAL;
+
+namespace LearnTest0316.BLL
+{
+    public class MVCAchPOC_DB_BLL
+    {
+        public static List<Users> getList() {
+            using (MvcAuthPOCEntities db =new MvcAuthPOCEntities()) {
+                List<Users> Users = db.Users.ToList();
+                return Users;
+            }
+        }
+    }
+}
---
+++ LearnTest0316/Controllers/FirstController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LearnTest0316.DAL;
+using LearnTest0316.BLL;
+
+namespace LearnTest0316.Controllers
+{
+    public class FirstController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Name_001 = "孫鵬鵬";
+            ViewBag.Name_002 = "林霏霏";
+            ViewBag.Data = Newtonsoft.Json.JsonConvert.SerializeObject(MVCAchPOC_DB_BLL.getList());
+            ViewData["Address_001"] = "臺灣_新北";
+            ViewData["Address_002"] = "韓國_首爾";
+            return View();
+        }
+        public ActionResult About(int Id) {
+            ViewBag.User_Id_001 = Id;
+            return View();
+        }
+    }
+}
